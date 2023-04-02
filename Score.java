af27ff2fc39f6bdf4a5413928c79dc1a8af53003
Nickmos41 <108105131+Nickmos41@users.noreply.github.com>
2023-04-02 19:00:39 -0400

    Update Score.java

--- Score.java
+++ Score.java
@@ -1,5 +1,11 @@
 /**
  * Grading Group Project
+ ***NEM***
+ There are multiple "grading schemas"
+ listed in the end of dcoumentation.
+ Writing this note to remember if one 
+ is preferred or if need to implement 
+ muliples.
  */
 
 public interface Score {
