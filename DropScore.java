997cf8ea5b8fbd85e51018a6b8b58e52e72bdcb9
BrandeGuess <127990991+BrandeGuess@users.noreply.github.com>
2023-04-13 16:17:15 -0400

    Update DropScore.java

--- DropScore.java
+++ DropScore.java
@@ -4,7 +4,32 @@
  * Drop lowest score from Quizzes & labs
  *
  */
-public class DropScore implements Score{
-	
-	public double getScore
+package Grading;
+
+import java.util.ArrayList;
+
+public class DropScore {
+    public static ArrayList<Score> dropLowest(ArrayList<Score> scores){
+        double lowest = scores.get(0).getScore();
+        for (int i = 1; i < scores.size(); i++){
+            if (scores.get(i).getScore() < lowest){
+                lowest = scores.get(i).getScore();
+            }
+        }
+        int pos = 0;
+        boolean found = false;
+        while (pos < scores.size() && !found){
+            if(scores.get(pos).getScore() == lowest){
+                found = true;
+            }
+            else{
+                pos++;
+            }
+        }
+        if(found) {
+            scores.remove(pos);
+        }
+        return scores;
+    }
+        
 }
