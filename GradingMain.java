e572940046eeceb46a45c4b73e9f837719a1dfe0
BrandeGuess <127990991+BrandeGuess@users.noreply.github.com>
2023-04-16 13:58:24 -0400

    Update GradingMain.java

--- GradingMain.java
+++ GradingMain.java
@@ -6,7 +6,12 @@ import java.util.Scanner;
 public class GradingMain {
     public static void main(String[] args) {
     	Scanner in = new Scanner(System.in);
-    	
+        System.out.println("Please enter the grade: ");
+    	in.nextDouble();
+    	while() {
+    		
+    	}
+    	in.close();
     }
-    in.close();
+    
 }
