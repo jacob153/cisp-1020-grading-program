b6ae1271d934a7007d22a739a5c2512a8c8edc67
BrandeGuess <127990991+BrandeGuess@users.noreply.github.com>
2023-04-25 16:27:16 -0400

    Update GradeWriter.java

--- src/GradeWriter.java
+++ src/GradeWriter.java
@@ -1,36 +1,49 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GradeWriter {
-    public GradeWriter() throws IOException{
-		try (Scanner in = new Scanner(System.in)) {
+    /**
+     * Prints student's grades to file
+     * @param student from Student class
+     * @param quizzes array list of quiz grades
+     * @param labAssignments array list of lab assignments
+     * @param projects array list of projects
+     * @param exams array list of exams
+     * @param attendance array list of attendance
+     * @throws IOException 
+     */
+    public static void printGrades(Student student, ArrayList<Assessment> quizzes,
+            ArrayList<Assessment> labAssignments, ArrayList<Assessment> projects, ArrayList<Assessment> exams,
+            ArrayList<Assessment> attendance) throws IOException {
+        File file = new File("StudentProfile.txt");
+        FileWriter fw = new FileWriter(file, true); //true makes the info stay in the text file
+        BufferedWriter bw = new BufferedWriter(fw);
+        
+        bw.write(student.getsFirstName() + " " + student.getsLastName() + " " + student.getStudentId() + '\n');
+        bw.write(arrayToString("Quizzes", quizzes) + '\n'
+                + arrayToString("Lab grade", labAssignments) + '\n'
+                + arrayToString("Project grade", projects) + '\n'
+                + arrayToString("Exams", exams) + '\n'
+                + arrayToString("Attendance", attendance) + '\n');
+        
+        bw.close();
+        fw.close();  
+    }
+        /**
+         * Changes Assessment to string
+         * @param label string name
+         * @param listing array list of Assessment
+         * @return 
+         */
+    public static String arrayToString(String label, ArrayList<Assessment>listing){
+        String printMe = listing + ":";
+        for(Assessment a: listing){
+            printMe = printMe + " " + String.valueOf(a.getScore());
         }
-		File file = new File("Grades.txt");
-		try (FileWriter fw = new FileWriter(file, true)) {
-        }
-		try (PrintWriter pw = new PrintWriter(file)) {
-        }
-        pw.write(Grade + ", " + sFirstName + ": " + studentId + '\n');
-		pw.write("Quiz grade: " + quizGrade + '\n'
-				+ "Lab grade: " + labGrade + '\n' 
-				+ "Project grade: " + proj + '\n' 
-				+ "Exam 1: "+ exam1 + '\n' 
-				+ "Exam 2: " + exam2 + '\n' 
-				+ "Attendance: " + atten + '\n'
-				+ "GPA: " + totalScore + '\n');
-		in.close();
-		pw.close();
+        return printMe;
     }
 }
-
-
-/*
- * use this at top of other class
- * private static GradeWriter GradeWriter;
- * 
- * Use this to direct to this class
- * GradeWriter = new GradeWriter();
- */
