36d704ddfe975f8127989dbc911d2f49748fd697
BrandeGuess <127990991+BrandeGuess@users.noreply.github.com>
2023-04-25 17:32:01 -0400

    Update GPA.java

--- src/GPA.java
+++ src/GPA.java
@@ -1,22 +1,18 @@
 
-
 public class GPA {
 
-
-
-
-public static double calculateGPA(ScoreList scores) {
-    double finalGrade = scores.getFinalGrade();
-    if (finalGrade >= 90.0) {
-        return 4.0;
-    } else if (finalGrade >= 80.0) {
-        return 3.0;
-    } else if (finalGrade >= 70.0) {
-        return 2.0;
-    } else if (finalGrade >= 60.0) {
-        return 1.0;
-    } else {
-        return 0.0;
+    public static double calculateGPA(ScoreList scores) {
+        double finalGrade = scores.getFinalGrade();
+        if (finalGrade >= 90.0) {
+            return 4.0;
+        } else if (finalGrade >= 80.0) {
+            return 3.0;
+        } else if (finalGrade >= 70.0) {
+            return 2.0;
+        } else if (finalGrade >= 60.0) {
+            return 1.0;
+        } else {
+            return 0.0;
+        }
     }
-}
 }
